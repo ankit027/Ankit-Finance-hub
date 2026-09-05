a683d0e2747135bdc7e45dc75de3542c3ddcfc36
ankit027 <66833288+ankit027@users.noreply.github.com>
2026-09-05 17:05:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   Paste your deployed Google Apps Script Web App URL below.
   Example: https://script.google.com/macros/s/XXXXX/exec
 */
-const API_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbwYIXL6HtbCW6QiSediymQGV_zySDfcd0f-f61zJ2ihqeIFJ4h1C_Ge6T_zlaVWw3-M/exec";
 
 let DB = {}, charts = {};
 
